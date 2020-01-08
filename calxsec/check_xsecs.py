48af19a50ea052eed740d3cb4fcfe4ede6815dad
Ludovic Michel Scyboz <lscyboz@draco03.mpcdf.mpg.de>
2020-01-08 17:48:18 +0100

    final check

--- calxsec/check_xsecs.py
+++ calxsec/check_xsecs.py
@@ -7,6 +7,9 @@ l = len(os.listdir(path))
 
 nfail = 0
 
+faillist = []
+lists = []
+
 for i,dirs in enumerate(os.listdir(path)):
 
   curdir = cwd+'/'+path+dirs
@@ -14,7 +17,6 @@ for i,dirs in enumerate(os.listdir(path)):
   hand = dirs.split('-')[0]
   PRmss = float(dirs.split('-Mst.')[1].split('-M')[0])
   PRcim = float(dirs.split('-M1.')[1].split('-mt')[0])
-
 
   print i+1, "/", l, " ---> Cross-sections for ", hand, ", mstop = ", PRmss, ", mchi = ", PRcim
   print " -----------------------------------------------------------------------------"
@@ -35,12 +37,26 @@ for i,dirs in enumerate(os.listdir(path)):
 
   if float(xsecemu0) > 1e-5 and float(xsecemu1) > 1e-5 and float(xsecmue0) > 1e-5 and float(xsecmue1) > 1e-5:
     print "    -> \033[92m OK \033[0m"
+    lists += [[hand, PRmss, PRcim, 1]]
   else:
     nfail += 1
     print "    -> \033[91m FAIL \033[0m"
+    lists += [[hand, PRmss, PRcim, 0]]
+    faillist += [[hand, PRmss, PRcim, 0]]
 
   print " #############################################################################\n"
   xsecs.close()
 
-print "\n\n Number of divergent ( i.e. tiny ) PS points: ", nfail, " / ", l
+realfail = 0
+for e in lists:
 
+  if e[2] == 1: continue
+  if e[0] == 'L': handto = 'R'
+  if e[0] == 'R': handto = 'L'
+
+  for j in lists:
+    if e[1] == j[1] and e[2] == j[2] and e[0] == handto:
+      if j[2] == 0: realfail += 1
+
+print "\n\n Number of divergent ( i.e. tiny ) PS points: ", nfail, " / ", l
+print " Number of double-handed divergent ( i.e. tiny ) PS points: ", realfail, " / ", l
